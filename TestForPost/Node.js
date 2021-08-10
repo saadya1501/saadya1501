1430adfbf5b6b5a312f129ff4f559d64863c54e6
saadya1501 <44800522+saadya1501@users.noreply.github.com>
2021-08-10 15:46:56 +0530

    first commit

---
+++ TestForPost/Node.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer((req, res) => {
+	let body = '';
+	if(req.method === 'GET' && req.url === '/')
+	{
+		res.writeHead(200, {'Content-Type' : 'text/html'});
+		fs.readFile('./index.html', 'UTF-8', (err, data) => {
+			if(err) throw err;
+			res.write(data);
+			res.end();
+		});
+    }
+    else if(req.method === 'POST')
+    {   
+        req.on('data', (data) => {
+            body += data;
+        });
+        req.on('end', () => {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write(body, () => {
+                res.end();
+            });
+        });
+    }
+    else
+    {
+        res.writeHead(404, {'Content-Type' : 'text/html'});
+        res.end(`<h1>404 ERROR could not find that Page</h1>`);
+    }
+}).listen(3000);
+console.log('Server is Running');
